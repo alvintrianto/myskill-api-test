469a207f126960129def216289269150b95503e4
alvintrianto <alvin.trianto97@gmail.com>
2022-08-14 17:50:47 +0700

    Add type of assert

--- test/welcomeTest.js
+++ test/welcomeTest.js
@@ -5,7 +5,14 @@ const app = require('../app');
 //Definisi assert 
 //Cara assert menggunakan chai
 describe('App', function(){
+    //Assert tulisan sesuai ekspektasi
     it('app should return welcome message', function(){
         assert.equal(app(), "welcome to QA course at mySkill")
+    })
+
+    //Assert tipe data
+    it('app data type is string', function(){
+        let response = app();
+        assert.typeOf(response,'string')
     })
 })
