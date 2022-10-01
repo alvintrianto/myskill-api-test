44557cf02543ca085d9e7e348f564a9559f488b9
alvintrianto <alvin.trianto97@gmail.com>
2022-10-01 19:01:28 +0700

    update chaiAsser.js

--- test/chaiAssert.js
+++ test/chaiAssert.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai')
 const request = require('supertest')
-const data = require('../data/userData')
+const data = require('../data/userData.js')
 
 
 describe('Chai Assert - Get Example', () => {
@@ -21,7 +21,7 @@ describe('Post Request Example', () => {
     const response = request ('https://petstore.swagger.io/v2')
     .post('/user')
     .send(data.CREATE_USER_DATA)
-    it('response status equal to 200', async () => {
+    it('response status equal to 200', async  () => {
         expect((await response).status).to.equal(200)
     })
     
